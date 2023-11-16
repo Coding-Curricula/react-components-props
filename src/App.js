640b8231cbb4bfc72fba45e8620fa0b6e76409ae
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-11-15 22:27:41 -0600

    first component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react'
 
+const Genre = ({ name, description }) => {
+    return (
+        <div>
+            <h2>{name}</h2>
+            <p>{description}</p>
+        </div>
+    )
+}
+
 export default function App() {
-  return (
-    <div>Free to edit</div>
-  )
+    return (
+        <div>
+            <h1>Movie App</h1>
+            <Genre
+                name="Science Fiction Films"
+                description="Sci fi movies use technology like fantasy to explore the human condition in new exciting ways."
+            />
+            <h3>Alien</h3>
+            <p>Year: 1979</p>
+            <h3>Back to the Future</h3>
+            <p>Year: 1985</p>
+        </div>
+    )
 }
